97296aa28064b53ba02c77628125ffb8c4cd4474
Apowalowski <acpowalowski@gmail.com>
2021-04-12 15:27:51 -0400

    Class Exercise

---
+++ Class exercise/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_exercise
+{
+    public class Car
+    {
+
+
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        //Constructors
+        public Car() //A special member method that has the same name as it's class
+        {
+
+        }
+        //fields
+        //properties
+        //methods
+
+        //Create a Make property of type string that is public
+        public string Make { get; set; } //get = read set = write
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+    }
+}
---
+++ Class exercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Class_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Ford";
+            myCar.Model = "Focus";
+            myCar.Year = 2021;
+
+            var mazda = new Car()
+            {
+                Make = "Mazda",
+                Model = "CX-5",
+                Year = 2014, 
+
+            };
+            var chevy = new Car("Chevy", "Impala", 2000);
+            var carList = new List<Car>() { myCar, mazda, chevy };
+
+            Console.WriteLine(carList.Capacity); 
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+        }
+    }
+}
